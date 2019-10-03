5006919efa021d571baba3a1f2056fac8ad39309
gomjellie <gomjellie@naver.com>
2019-10-03 22:46:16 +0900

    add recursive factorial

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+
+iter:
+	gcc -o fact_itor fact_iter.c
+
+recursive:
+	gcc -o fact_recursive fact_recursive.c
+
 all: fact_iter.c
 	gcc -Og -o fact_iter fact_iter.c
 
 clean:
 	rm fact_iter
+
---
+++ fact_recursive.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int fact_recursive(int parent) {
+    /**
+     * 2! == 2
+     * 1! == 1
+     * 0! == 1
+     * -1 == Err
+     */
+    if (parent < 2) {
+        return 1;
+    }
+
+    return fact_recursive(parent - 1) * parent;
+}
+
+int main(int argc, char* argv[]) {
+    /**
+     * pass number argument like this
+     * ./fact_recursive 10
+     *
+     * number argument must be same or bigger than 0
+     * 
+     * potential bug: overflow
+     */
+	int input_number, res, i;
+	
+	if(argc < 2) {
+		puts("Usage: a.out num");
+		puts("E.g.: ./a.out 10\n");
+		return 0;
+	}
+	input_number = atoi(argv[1]);
+
+    assert(input_number >= 0);
+
+    res = fact_recursive(input_number);
+	printf("n's factorial = %d\n", res);
+	
+    return 0;
+}
+
