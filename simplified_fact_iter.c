fae518a126cf1088b110c513e70fbd380a0b8abb
gomjellie <gomjellie@naver.com>
2019-10-04 15:39:26 +0900

    어셈 코드 넘 복잡해서 C코드 줄여서 덤프

---
+++ simplified_fact_iter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int n, fact, i;
+	
+	n = atoi(argv[1]);
+
+	fact = 1;
+
+	for(i = 1; i <= n; i++) {
+		fact = fact * i;	
+	}
+	printf("%d\n", fact);
+	return 0;
+}
+
