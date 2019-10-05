b2f30d46a7ef24d73510224c26089dea1c92d06f
Jacky <gomjellie@gmail.com>
2019-10-05 22:40:33 +0900

    add .s asm files

--- simplified_fact_iter.c
+++ simplified_fact_iter.c
@@ -1,8 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	int n, fact, i;
 	
 	n = atoi(argv[1]);
@@ -15,4 +14,3 @@ int main(int argc, char* argv[])
 	printf("%d\n", fact);
 	return 0;
 }
-
